dfb5da8a5bc1e559948b3c51f8610d057d60e287
fecha <fecbatino@gmail.com>
2026-05-25 15:28:02 +0200

    fix: use HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext';
 import { SettingsProvider } from './contexts/SettingsContext';
 import './i18n';
@@ -129,7 +129,7 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <SettingsProvider>
         <AuthProvider>
           <ThemeProvider theme={theme}>
@@ -142,6 +142,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           </ThemeProvider>
         </AuthProvider>
       </SettingsProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
